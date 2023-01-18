10f91dd1d3a8fb2c926320fcac16eeca093b62c2
Sunanda1234567 <2000031095@kluniversity.in>
2023-01-18 08:40:55 +0530

    version1

---
+++ JenkinsJavaFileToGit.java
@@ -0,0 +1,13 @@
+package jenkinsGit;
+public class JenkinsJavaFileGit{
+public static void main(String[] args){
+int a=10;
+int b=30;
+a=a+b;
+b=a-b;
+a=a-b;
+System.out.println(a);
+System.out.println(b);
+
+}
+}
